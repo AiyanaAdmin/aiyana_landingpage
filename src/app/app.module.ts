2a438348b32a58f7d6f98d3dc57b995e8f0207cc
unknown <andreas.benz.development@outlook.de>
2019-06-17 04:03:11 +0200

    add c03 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { C01Component } from './c01.component/c01.component';
 import { C02Component } from './c02.component/c02.component';
+import { C03Component } from './c03.component/c03.component';
 @NgModule({
   declarations: [
     AppComponent,
     C01Component,
-    C02Component
+    C02Component,
+    C03Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/c03.component/c03.component.ts
@@ -0,0 +1,26 @@
+/**
+*  Document:  c03.component.ts
+*
+*  Author:    andreas.benz.development@outlook.de
+*
+*  Owner:     andreas.benz.development@outlook.de
+*
+*  History:
+*
+*  17.06.19 initial build
+*/
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c03',
+  templateUrl: './c03.component.html',
+  styleUrls: ['./c03.component.scss']
+})
+export class C03Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
